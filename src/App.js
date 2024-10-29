ce5ab925ad79805500e0afed66f1a9596c8ee5c1
Jola <nitariod@gmail.com>
2024-10-29 03:24:14 +0100

    add sign in component, firebase utils and brings in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
-import categories from "./data/categories.json";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the second home</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
   );
 };
 
