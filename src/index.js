c1ff09de9e943726d6680ba3919754281297a702
Jola <nitariod@gmail.com>
2024-12-03 22:29:22 +0100

    add cart context, shopping icon and cart dropdown

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./context/user.context";
+import { ProductProvider } from "./context/products.context";
+import { CartProvider } from "./context/cart.context";
 import reportWebVitals from "./reportWebVitals";
 
 import "./index.scss";
 
-ReactDOM.render(
+const root = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,11 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
@@ -28,7 +32,9 @@ const Navigation = () => {
               SIGNIN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
